eb1f7848111ada4bd8378505ca4672e781f313e9
Robert Ramos Jr <Robert.ramosjr214@gmail.com>
2020-02-20 18:37:52 -0600

    Api3 Project

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // code away!
+const express = require('express')
+const server = express()
+const logger = require('./middleware/logger')
+const validate = require('./middleware/validate')
+
+const userRouter = require('./users/userRouter')
+
+server.use(logger('long')) 
+server.use(express.json())
+server.use("/", (req, res) => {
+    res
+    .json({ message: "Welcome to the Node API3 project!"})
+})
+
+server.use("/api/users", userRouter)
+
+server.use((req, res) => {
+    res
+    .status(404)
+    .json({ message: "Route was not found." })
+})
+server.use((err, req, res, next) => {
+    console.log(err)
+    res
+        .status(500)
+        .json({ message: "An internal error occurred." })
+    })
+
+const port = 8080
+const host = "127.0.0.1"
+
+server.listen(port, host, () => {
+    console.log(`Server running on ${host}:${port}`)
+})
--- users/userRouter.js
+++ users/userRouter.js
@@ -1,47 +1,91 @@
 const express = require('express');
+const users = require('./userDb')
+const postRouter = require('../posts/postRouter')
 
 const router = express.Router();
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // do your magic!
 });
 
-router.post('/:id/posts', (req, res) => {
+router.post('/:id/posts', async (req, res) => {
   // do your magic!
 });
 
-router.get('/', (req, res) => {
+router.get('/', (req, res, next) => {
+  users.get()
+    .then(users => {
+      res
+        .status(200)
+        .json(users)
+    })
+    .catch(error => {
+      next(error)
+    })
   // do your magic!
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', validateUserId(), async (req, res) => {
   // do your magic!
 });
 
-router.get('/:id/posts', (req, res) => {
+router.get('/:id/posts', async (req, res) => {
   // do your magic!
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // do your magic!
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // do your magic!
 });
 
 //custom middleware
 
 function validateUserId(req, res, next) {
-  // do your magic!
+  return (req, res, next) => {
+    users.findById(req.params.id)
+    then(user => {
+      if (user) {
+        req.user = user
+          console.log("User id is valid.")
+          next()
+        } else {
+          res
+          .status(400)
+          .json({ message: "Invalid user id." })
+        }
+      })
+  }
 }
 
 function validateUser(req, res, next) {
-  // do your magic!
+  return (req, res, next) => {
+    if(!req.body) {
+      return res
+              .status(400)
+              .json({ message: "Missing user data"})
+    }
+    next()
+  }
 }
 
 function validatePost(req, res, next) {
   // do your magic!
+  return (req, res, next) => {
+    if(!req.body) {
+      return res
+              .status(400)
+              .json({ message: "Missing post data"})
+    }
+    if(!req.body.text) {
+      return res
+              .status(400)
+              .json({ message: "Missing required text field." })
+    }
+    next()
+  }
 }
 
 module.exports = router;
